f92421b75ffed1715fe0e22ef5dd3f4e6bd5815c
Tobias Schlitt <toby@php.net>
2012-10-30 16:34:50 +0100

    Fixed: Don't use Behat Symfony extension for simplicity reasons.

---
+++ features/Context/FeatureContext.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Qafoo\CfpBundle\Features\Context;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\MinkExtension\Context\MinkContext;
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Feature context.
+ */
+class FeatureContext extends MinkContext
+{
+}
